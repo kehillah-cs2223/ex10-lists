ede43cab4e3f0ee6d1127af709f9331efaed8a89
Erin Bennett <ebennett@kehillah.org>
2023-02-06 09:19:25 -0800

    add tests

--- q4.py
+++ q4.py
@@ -1,2 +1,22 @@
-# make a list of 100 file names with the format "file_0.txt"
-# hint: use a loop and append
+def make_list_of_files(n):
+    """
+    make a list of n file names with the format "file_0.txt"
+
+    assume n greater than or equal to 1
+
+    hint: use a loop and append
+
+    >>> make_list_of_files(10)
+    ['file_0.txt', 'file_1.txt', 'file_2.txt', 'file_3.txt', 'file_4.txt', 'file_5.txt', 'file_6.txt', 'file_7.txt', 'file_8.txt', 'file_9.txt']
+    >>> make_list_of_files(1)
+    ['file_0.txt']
+    >>> make_list_of_files(5)
+    ['file_0.txt', 'file_1.txt', 'file_2.txt', 'file_3.txt', 'file_4.txt']
+    """
+    return
+
+if __name__ == "__main__":
+    import doctest
+    print("Running tests...")
+    doctest.testmod()
+    print("All tests have finished!")
