823ed79599d32c4bc24fb04eee98c184707de44d
Erin Bennett <ebennett@kehillah.org>
2023-02-06 09:09:59 -0800

    list questions

---
+++ q1.py
@@ -0,0 +1,11 @@
+# a)
+# Create a list with the following five items:
+# 7, 9, "a", "cat", False
+# Assign this list to the variable my_list
+
+
+# b) Append 3.14 and 7 to the list
+
+
+# c) Find the index of "cat"
+
---
+++ q3.py
@@ -0,0 +1,17 @@
+def average(numbers):
+    """
+    return the average of a list of numbers
+    >>> average([32, 78, 48, 71, 93, 71, 79, 44, 5, 42])
+    56.3
+    >>> average([5, 4, 3, 2, 1])
+    3.0
+    >>> average([8.0, 3.14159, 17])
+    9.38053
+    """
+    return
+
+if __name__ == "__main__":
+    import doctest
+    print("Running tests...")
+    doctest.testmod()
+    print("All tests have finished!")
